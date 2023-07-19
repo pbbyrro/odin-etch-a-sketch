f43ea0a1b15d3fba05c63716da8c2613865c9b63
Patricia Byrro <pbbyrro@gmail.com>
2023-07-19 13:34:31 -0300

    createBoard function

---
+++ main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function()) {
+    
+}
+
+function createBoard(size) {
+    let board = document.querySelector(".board");
+
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let numDivs = size * size;
+    for (let i = 0; i < numDivs; i++) {
+        let div = document.createElement("div");
+        board.insertAdjacentElement("beforeend", div);
+    }
+}
